0e22e941515f39f3c52252e30f856e6f2a421d7b
Richard Smith <richard.m.smith@ons.gov.uk>
2023-03-03 11:45:20 +0000

    custom constraintvalidatorfactory

--- src/main/java/uk/gov/ons/bulk/validator/EpochValidator.java
+++ src/main/java/uk/gov/ons/bulk/validator/EpochValidator.java
@@ -10,17 +10,24 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class EpochValidator implements ConstraintValidator<Epoch, String> {
 
-	public EpochValidator(){}
+	public EpochValidator(String epochs){
+		this.altEpochs = epochs;
+	}
 
 	@Value("${aims.epochs}")
 	private String epochs;
+
+	private String altEpochs;
 	
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
 
-	//	System.out.println("epochs = " + epochs);
+		System.out.println("epochs = " + epochs);
+		System.out.println("altEpochs = " + epochs);
+		String testEpochs = epochs;
+		if (epochs.isEmpty()) testEpochs = altEpochs;
 
-		Pattern pattern = Pattern.compile(String.format("^(%s)$", epochs)); //^(95|94|93|92)$
+		Pattern pattern = Pattern.compile(String.format("^(%s)$", testEpochs)); //^(98|97|96|95|94|93)$
 		Matcher matcher = pattern.matcher(value);
 		
 		return matcher.matches();
--- src/test/java/uk/gov/ons/bulk/entities/PayloadValidationTests.java
+++ src/test/java/uk/gov/ons/bulk/entities/PayloadValidationTests.java
@@ -1,9 +1,11 @@
 package uk.gov.ons.bulk.entities;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
@@ -11,16 +13,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorFactory;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import uk.gov.ons.bulk.validator.Epoch;
+import uk.gov.ons.bulk.CustomLocalValidatorFactoryBean;
+import uk.gov.ons.bulk.service.BulkStatusService;
+import uk.gov.ons.bulk.service.IdsService;
+import uk.gov.ons.bulk.validator.EpochValidator;
 
 @SpringBootTest()
 @ExtendWith(SpringExtension.class)
@@ -32,15 +47,44 @@ public class PayloadValidationTests {
     @Value("${aims.current-epoch}")
     private String currentEpoch;
 
+    @Value("${aims.epochs}")
+    private String epochs;
+
+     private final List<ConstraintValidator<?,?>> customConstraintValidators =
+            Collections.singletonList(new EpochValidator(epochs));
+    private final ValidatorFactory customValidatorFactory =
+            new CustomLocalValidatorFactoryBean(customConstraintValidators);
+    private final Validator validator = customValidatorFactory.getValidator();
+
+   // private static Validator validator;
+
+   // @Autowired
+   // private static IdsService idsservice;
+
+//    @BeforeClass
+//    public static void setUp() {
+//        ConstraintValidatorFactory cvf = mock(ConstraintValidatorFactory.class);
+//        when(cvf.getInstance(EpochValidator.class)).thenReturn(new EpochValidator(idsservice));
+//
+//        validator = Validation.buildDefaultValidatorFactory()
+//                .usingContext()
+//                .constraintValidatorFactory(cvf)
+//                .getValidator();
+//    }
+
     @Test
     public void testPayloadValidatorHappy() throws Exception {
+
+        System.out.println("epochs="+epochs);
 
         ObjectMapper objectMapper = new ObjectMapper();
         NewIdsJobPayload testPayload = objectMapper.readValue(new File("src/test/resources/message-new-ids-payload-happy.json"),
                 NewIdsJobPayload.class);
 
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
+//        ConstraintValidatorFactory cvf = mock(ConstraintValidatorFactory.class);
+//        when(cvf.getInstance(EpochValidator.class)).thenReturn(new EpochValidator());
+//
+//        Validator evalidator = Validation.buildDefaultValidatorFactory().usingContext().constraintValidatorFactory(cvf).getValidator();
         Set<ConstraintViolation<NewIdsJobPayload>> violations = validator.validate(testPayload);
         StringBuilder validationErrorMessages = new StringBuilder("");
         for (ConstraintViolation<NewIdsJobPayload> violation : violations) {
