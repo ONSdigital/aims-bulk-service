978038e41c51e547a9ad31a6c1bc527cafeeaa80
Richard Smith <richard.m.smith@ons.gov.uk>
2024-02-15 13:14:25 +0000

    bump epoch to 106 and version to 0.91

--- src/main/java/uk/gov/ons/bulk/validator/EpochValidator.java
+++ src/main/java/uk/gov/ons/bulk/validator/EpochValidator.java
@@ -37,7 +37,7 @@ public class EpochValidator implements ConstraintValidator<Epoch, String> {
 
 		log.debug("Epochs: " + epochs);
 
-		Pattern pattern = Pattern.compile(String.format("^(%s)$", epochs)); //^(105|104|103|102|101|99|97|95)$
+		Pattern pattern = Pattern.compile(String.format("^(%s)$", epochs)); //^(106|105|104|103|102|101|99|97|95)$
 		Matcher matcher = pattern.matcher(value);
 		
 		return matcher.matches();
