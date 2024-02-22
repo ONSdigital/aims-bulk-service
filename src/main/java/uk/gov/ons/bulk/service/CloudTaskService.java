61139f6496ab930c20a76b90fe9f744de79e3dae
steve-thorne-ONS <steven.thorne@ons.gov.uk>
2024-02-22 11:12:04 +0000

    Turn on exponential backoff.
    This is for the request to the create tasks function. This should help
    on cold starts. Defaults set at present but can be tweaked in future.
    Fix broken test and some formatting updates.

--- src/main/java/uk/gov/ons/bulk/service/CloudTaskService.java
+++ src/main/java/uk/gov/ons/bulk/service/CloudTaskService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpBackOffUnsuccessfulResponseHandler;
 import com.google.api.client.http.HttpContent;
 import com.google.api.client.http.HttpHeaders;
 import com.google.api.client.http.HttpRequest;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.http.json.JsonHttpContent;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.ExponentialBackOff;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.IdTokenCredentials;
@@ -35,105 +37,132 @@ public class CloudTaskService {
 
 	@Value("${aims.cloud-functions.create-cloud-task-function}")
 	private String createTaskFunction;
-	
+
 	@Value("${aims.report-frequency}")
-	private double reportFrequency; 
-	
+	private double reportFrequency;
+
+	@Value("${exponential-backoff.initial-interval-millis}")
+	private int initialIntervalMillis;
+
+	@Value("${exponential-backoff.max-elapsed-time-millis}")
+	private int maxElapsedTimeMillis;
+
+	@Value("${exponential-backoff.max-interval-millis}")
+	private int maxIntervalMillis;
+
+	@Value("${exponential-backoff.multiplier}")
+	private double multiplier;
+
+	@Value("${exponential-backoff.randomization-factor}")
+	private double randomizationFactor;
+
 	private GenericUrl genericUrl;
 	private HttpCredentialsAdapter adapter;
-	private HttpTransport transport;		
+	private HttpTransport transport;
 
 	public void init() throws IOException {
 		GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
-		
+
 		if (!(credentials instanceof IdTokenProvider)) {
 			throw new IllegalArgumentException("Credentials are not an instance of IdTokenProvider.");
 		}
-		
+
 		IdTokenCredentials tokenCredential = IdTokenCredentials.newBuilder()
 				.setIdTokenProvider((IdTokenProvider) credentials).setTargetAudience(createTaskFunction).build();
 
 		genericUrl = new GenericUrl(createTaskFunction);
 		adapter = new HttpCredentialsAdapter(tokenCredential);
-		transport = new NetHttpTransport();	
+		transport = new NetHttpTransport();
 	}
 
 	/**
-	 * Send each address to GCP Cloud Function for matching.
-	 * Done asynchronously so as not to block the client response.
-	 * This won't work locally as it requires a service account.
-	 * Service account has Cloud Functions Invoker role but must also authenticate. 
+	 * Send each address to GCP Cloud Function for matching. Done asynchronously so
+	 * as not to block the client response. This won't work locally as it requires a
+	 * service account. Service account has Cloud Functions Invoker role but must
+	 * also authenticate.
 	 * 
 	 * @param jobId
 	 * @param addresses
 	 * @throws IOException
-	 */	
+	 */
 	@Async
-	public void createTasks(long jobId, BulkRequest[] addresses, long totalAddresses, BulkRequestParams bulkRequestParams, HttpHeaders headers) throws IOException {
-			
+	public void createTasks(long jobId, BulkRequest[] addresses, long totalAddresses,
+			BulkRequestParams bulkRequestParams, HttpHeaders headers) throws IOException {
+
 		List<Integer> reportAddresses = reportAddresses(addresses.length);
 		reportAddresses.add(addresses.length);
 		init();
-		
-		for (int i = 0; i < addresses.length; i++) {	
-					
+
+		ExponentialBackOff backoff = new ExponentialBackOff.Builder().setInitialIntervalMillis(initialIntervalMillis)
+				.setMaxElapsedTimeMillis(maxElapsedTimeMillis).setMaxIntervalMillis(maxIntervalMillis)
+				.setMultiplier(multiplier).setRandomizationFactor(randomizationFactor).build();
+
+		for (int i = 0; i < addresses.length; i++) {
+
 			if (reportAddresses.contains(i + 1)) {
 				log.debug("Reporting: " + (i + 1));
 			}
-			
-			BulkJobRequest bjr = new BulkJobRequest(String.valueOf(jobId), "", addresses[i].getId(), addresses[i].getAddress(), 
-					String.valueOf(i + 1), String.valueOf(totalAddresses), 
+
+			BulkJobRequest bjr = new BulkJobRequest(String.valueOf(jobId), "", addresses[i].getId(),
+					addresses[i].getAddress(), String.valueOf(i + 1), String.valueOf(totalAddresses),
 					String.valueOf(reportAddresses.contains(i + 1)), bulkRequestParams);
-			
+
 			HttpContent content = new JsonHttpContent(new GsonFactory(), bjr.getJob());
 			HttpRequest request = transport.createRequestFactory(adapter).buildPostRequest(genericUrl, content);
+			request.setUnsuccessfulResponseHandler(new HttpBackOffUnsuccessfulResponseHandler(backoff));
 			request.setHeaders(headers);
 			request.execute();
 		}
 	}
-	
+
 	@Async
-	public void createIdsTasks(long jobId, String idsJobId, List<IdsRequest> addresses, long totalAddresses, 
+	public void createIdsTasks(long jobId, String idsJobId, List<IdsRequest> addresses, long totalAddresses,
 			BulkRequestParams bulkRequestParams, HttpHeaders headers) throws IOException {
-		
+
 		List<Integer> reportAddresses = reportAddresses(addresses.size());
 		reportAddresses.add(addresses.size());
 		init();
-		
-		for (int i = 0; i < addresses.size(); i++) {	
-					
+
+		ExponentialBackOff backoff = new ExponentialBackOff.Builder().setInitialIntervalMillis(initialIntervalMillis)
+				.setMaxElapsedTimeMillis(maxElapsedTimeMillis).setMaxIntervalMillis(maxIntervalMillis)
+				.setMultiplier(multiplier).setRandomizationFactor(randomizationFactor).build();
+
+		for (int i = 0; i < addresses.size(); i++) {
+
 			if (reportAddresses.contains(i + 1)) {
 				log.debug("Reporting: " + (i + 1));
 			}
-			
-			BulkJobRequest bjr = new BulkJobRequest(String.valueOf(jobId), idsJobId, addresses.get(i).getId(), addresses.get(i).getAddress(), 
-					String.valueOf(i + 1), String.valueOf(totalAddresses), 
+
+			BulkJobRequest bjr = new BulkJobRequest(String.valueOf(jobId), idsJobId, addresses.get(i).getId(),
+					addresses.get(i).getAddress(), String.valueOf(i + 1), String.valueOf(totalAddresses),
 					String.valueOf(reportAddresses.contains(i + 1)), bulkRequestParams);
-			
+
 			HttpContent content = new JsonHttpContent(new GsonFactory(), bjr.getJob());
 			HttpRequest request = transport.createRequestFactory(adapter).buildPostRequest(genericUrl, content);
+			request.setUnsuccessfulResponseHandler(new HttpBackOffUnsuccessfulResponseHandler(backoff));
 			request.setHeaders(headers);
 			request.execute();
 		}
 	}
-	
+
 	private List<Integer> reportAddresses(double length) {
-		
+
 		double x = reportFrequency / 100;
 		List<Integer> reportAddresses = new ArrayList<Integer>();
-		
+
 		do {
-			reportAddresses.add((int)Math.round(length * x));
+			reportAddresses.add((int) Math.round(length * x));
 			x = x + (reportFrequency / 100);
-		} while(x < 1);
-		
+		} while (x < 1);
+
 		return reportAddresses;
 	}
-	
+
 	public @Data class BulkJobRequest {
 		private Map<String, String> job;
 
-		public BulkJobRequest(String jobId, String idsJobId, String id, String address, String addressNumber, String totalAddresses, String report, BulkRequestParams bulkRequestParams) {
+		public BulkJobRequest(String jobId, String idsJobId, String id, String address, String addressNumber,
+				String totalAddresses, String report, BulkRequestParams bulkRequestParams) {
 			super();
 			job = new HashMap<String, String>();
 			job.put("jobId", jobId);
