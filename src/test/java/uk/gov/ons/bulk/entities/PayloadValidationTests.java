8c9f227c4d7887deb0cbc737cb8da3991ccfb80b
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-04-03 14:18:55 +0100

    Removed old epoich numbers, leaving 107, 106, 105 to be in line with the actual allowed Epochs

--- src/test/java/uk/gov/ons/bulk/entities/PayloadValidationTests.java
+++ src/test/java/uk/gov/ons/bulk/entities/PayloadValidationTests.java
@@ -88,7 +88,7 @@ public class PayloadValidationTests {
 			validationErrorMessages.add(violation.getMessage());
 		});
 
-        String expectedMsg =  "epoch_number must be one of 107, 106, 105, 103, 102, 101, 99, 97, 95";
+        String expectedMsg =  "epoch_number must be one of 107, 106, 105";
         assertEquals(1, validationErrorMessages.size());
         assertTrue(validationErrorMessages.contains(expectedMsg));
     }
