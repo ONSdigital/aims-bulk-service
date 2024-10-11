aac4c39c3bffd6a700961e5578ee4e39db7150a4
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-10-11 12:36:17 +0100

    Make test public

--- src/test/java/uk/gov/ons/bulk/util/BulkPropertiesTest.java
+++ src/test/java/uk/gov/ons/bulk/util/BulkPropertiesTest.java
@@ -4,15 +4,17 @@ import org.junit.Test;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@SpringBootTest
 class BulkPropertiesTest implements BulkProperties{
+
   @Test
-  void missingProperty() {
+  public void missingProperty() {
     Throwable exception = assertThrows(AssertionError.class, () -> {
       BulkProperties.getYamlProperty("not.here");
     });
@@ -20,12 +22,12 @@ class BulkPropertiesTest implements BulkProperties{
   }
 
   @Test
-  void existingProperty() {
+  public void existingProperty() {
     assertEquals("aims-bulk-service", BulkProperties.getYamlProperty("spring.application.name"));
   }
 
   @Test
-  void failingTest() {
+  public void failingTest() {
     assertEquals("aims-FAILEDvice", BulkProperties.getYamlProperty("spring.application.name"));
   }
 }
