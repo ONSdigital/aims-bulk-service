a8d301a678df1d26aa1500f26d41bab80282e5d4
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-10-11 16:08:40 +0100

    Removed deliberately failing test. Removed un-needed @SpringBootTest. Changed the Test import which now makes the tests in BulkPropertiesTest.java run.

--- src/test/java/uk/gov/ons/bulk/util/BulkPropertiesTest.java
+++ src/test/java/uk/gov/ons/bulk/util/BulkPropertiesTest.java
@@ -1,16 +1,9 @@
 package uk.gov.ons.bulk.util;
 
-import org.junit.Test;
-import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SuiteDisplayName;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 class BulkPropertiesTest implements BulkProperties{
 
   @Test
@@ -18,16 +11,11 @@ class BulkPropertiesTest implements BulkProperties{
     Throwable exception = assertThrows(AssertionError.class, () -> {
       BulkProperties.getYamlProperty("not.here");
     });
-    assertTrue(exception.getMessage().contains("Property: not.here - not found!"));
+    assertTrue(exception.getMessage().contains("Property 'not.here' not found in application.yml"));
   }
 
   @Test
   public void existingProperty() {
     assertEquals("aims-bulk-service", BulkProperties.getYamlProperty("spring.application.name"));
-  }
-
-  @Test
-  public void failingTest() {
-    assertEquals("aims-FAILEDvice", BulkProperties.getYamlProperty("spring.application.name"));
   }
 }
