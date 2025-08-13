fd2aa36fb1e1c32d37282f4fbbd657fcd6db7ea2
RichardSmithONS <richard.m.smith@ons.gov.uk>
2025-08-13 09:16:18 +0100

    Bump Epoch to 118, drop 114

--- src/test/java/uk/gov/ons/bulk/controllers/BulkAddressApplicationTest.java
+++ src/test/java/uk/gov/ons/bulk/controllers/BulkAddressApplicationTest.java
@@ -506,9 +506,9 @@ public class BulkAddressApplicationTest {
 				.content(new ObjectMapper().writeValueAsString(bulkRequestContainer)).param("epoch", "100")
 				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isBadRequest())
 				.andExpect(jsonPath("$.status", Is.is("BAD_REQUEST")))
-				.andExpect(jsonPath("$.message", containsString("epoch_number must be one of 117, 116, 114, 99")))
+				.andExpect(jsonPath("$.message", containsString("epoch_number must be one of 118, 117, 116, 99")))
 				.andExpect(jsonPath("$.errors").isArray()).andExpect(jsonPath("$.errors", hasSize(1)))
-				.andExpect(jsonPath("$.errors", hasItem(containsString("epoch_number must be one of 117, 116, 114, 99"))))
+				.andExpect(jsonPath("$.errors", hasItem(containsString("epoch_number must be one of 118, 117, 116, 99"))))
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
 	}
 	
@@ -578,7 +578,7 @@ public class BulkAddressApplicationTest {
 			@RequestBody BulkRequestContainer bulkRequestContainer) throws Exception {
 		
 		String classError = "runBulkRequest.classificationfilter: classificationfilter may not contain a list and/or a wildcard";
-		String epochError = "runBulkRequest.epoch: epoch_number must be one of 117, 116, 114, 99";
+		String epochError = "runBulkRequest.epoch: epoch_number must be one of 118, 117, 116, 99";
 		String excludeenglandError = "runBulkRequest.excludeengland: excludeengland must be true or false";
 		String excludenorthernirelandError = "runBulkRequest.excludenorthernireland: excludenorthernireland must be true or false";
 		
