22835e44cfaef4bed866821eec748fd0cc4230bf
steve-thorne-ONS <steven.thorne@ons.gov.uk>
2026-09-24 20:49:48 +0100

    Remove IDS code.

--- src/main/java/uk/gov/ons/bulk/service/BulkStatusService.java
+++ src/main/java/uk/gov/ons/bulk/service/BulkStatusService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import uk.gov.ons.bulk.entities.BulkInfo;
-import uk.gov.ons.bulk.entities.IdsBulkInfo;
 import uk.gov.ons.bulk.repository.BulkStatusRepository;
 import uk.gov.ons.bulk.util.BulkServiceConstants.Status;
 
@@ -26,25 +25,5 @@ public class BulkStatusService {
 
 	public List<BulkInfo> getJobs(String userid, String status) {
 		return bulkStatusRepository.getJobs(userid, status);
-	}
-	
-	public Long saveIdsJob(IdsBulkInfo job) {
-		return bulkStatusRepository.saveIdsJob(job);
-	}
-	
-	public List<IdsBulkInfo> queryIdsJob(long jobId) {
-		return bulkStatusRepository.queryIdsJob(jobId);
-	}
-
-	public List<IdsBulkInfo> getIdsJobs(String userid, String status) {
-		return bulkStatusRepository.getIdsJobs(userid, status);
-	}
-	
-	public List<IdsBulkInfo> getIdsJob(String idsJobId) {
-		return bulkStatusRepository.getIdsJob(idsJobId);
-	}
-	
-	public void updateStatus(long jobId, Status status) {
-		bulkStatusRepository.updateStatus(jobId, status);
 	}
 }
--- src/test/java/uk/gov/ons/bulk/repository/BulkStatusRepositoryTest.java
+++ src/test/java/uk/gov/ons/bulk/repository/BulkStatusRepositoryTest.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.ons.bulk.entities.BulkInfo;
-import uk.gov.ons.bulk.entities.IdsBulkInfo;
-import uk.gov.ons.bulk.util.BulkServiceConstants.Status;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -41,16 +39,6 @@ class BulkStatusRepositoryTest {
 		
 		assertThat(result == 1L);
 	}
-	
-	@Test
-	public void testSaveIdsJob() {
-		
-		IdsBulkInfo idsBulkInfo = new IdsBulkInfo("ids-job-xx", "ids-user-xx", "in-progress", 107, 0, false);
-		idsBulkInfo.setStartdate(ldt1);
-		Long result = bulkStatusRepository.saveJob(idsBulkInfo);
-		
-		assertThat(result == 1L);
-	}
     
 	@Test
 	public void testQueryJob() {
@@ -58,19 +46,6 @@ class BulkStatusRepositoryTest {
 		BulkInfo result = bulkStatusRepository.queryJob(1).getFirst();
 		assertThat(result.getJobid()).isEqualTo(1);
 		assertThat(result.getUserid()).isEqualTo("bob");
-		assertThat(result.getStatus()).isEqualTo("in-progress");
-		assertThat(result.getTotalrecs()).isEqualTo(107);
-		assertThat(result.getRecssofar()).isEqualTo(45);
-		assertThat(result.getStartdate()).isEqualTo(ldt1.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-	}
-	
-	@Test
-	public void testQueryIdsJob() {
-
-		IdsBulkInfo result = bulkStatusRepository.queryIdsJob(1).getFirst();
-		assertThat(result.getJobid()).isEqualTo(1);
-		assertThat(result.getIdsJobId()).isEqualTo("ids-job-1");
-		assertThat(result.getUserid()).isEqualTo("ids-user-x");
 		assertThat(result.getStatus()).isEqualTo("in-progress");
 		assertThat(result.getTotalrecs()).isEqualTo(107);
 		assertThat(result.getRecssofar()).isEqualTo(45);
@@ -91,57 +66,5 @@ class BulkStatusRepositoryTest {
 				
 		assertTrue(bulkInfos.stream().map(BulkInfo::getTotalrecs).toList().containsAll(List.of(348076L, 107L)));
 		assertTrue(bulkInfos.stream().map(BulkInfo::getRecssofar).toList().containsAll(List.of(2000L, 45L)));
-	}
-	
-	@Test
-	public void testgetIdsJobs() {
-
-		List<IdsBulkInfo> idsBulkInfos = bulkStatusRepository.getIdsJobs("ids-user-y","in-progress");
-		
-		assertEquals(2, idsBulkInfos.size());
-		
-		idsBulkInfos.forEach(bulkInfo -> {	
-			assertEquals("ids-user-y", bulkInfo.getUserid());
-			assertEquals("in-progress", bulkInfo.getStatus());
-		});
-				
-		assertTrue(idsBulkInfos.stream().map(IdsBulkInfo::getIdsJobId).toList().containsAll(List.of("ids-job-2", "ids-job-3")));
-		assertTrue(idsBulkInfos.stream().map(BulkInfo::getTotalrecs).toList().containsAll(List.of(348076L, 107L)));
-		assertTrue(idsBulkInfos.stream().map(BulkInfo::getRecssofar).toList().containsAll(List.of(2000L, 45L)));
-	}
-	
-	@Test
-	public void testgetIdsJob() {
-
-		List<IdsBulkInfo> idsBulkInfos = bulkStatusRepository.getIdsJob("ids-job-2");
-		
-		assertEquals(1, idsBulkInfos.size());
-		assertEquals("ids-job-2", idsBulkInfos.getFirst().getIdsJobId());
-		assertEquals("ids-user-y", idsBulkInfos.getFirst().getUserid());
-		assertEquals("in-progress", idsBulkInfos.getFirst().getStatus());
-		assertEquals(2000L, idsBulkInfos.getFirst().getRecssofar());
-		assertEquals(348076L, idsBulkInfos.getFirst().getTotalrecs());
-	}
-	
-	@Test
-	public void testgetNonExistentIdsJob() {
-
-		List<IdsBulkInfo> idsBulkInfos = bulkStatusRepository.getIdsJob("ids-job-99");
-		assertEquals(0, idsBulkInfos.size());
-	}
-	
-	@Test
-	public void testUpdateStatus() {
-		
-		bulkStatusRepository.updateStatus(2L, Status.RD);
-				
-		IdsBulkInfo result = bulkStatusRepository.queryIdsJob(2L).getFirst();
-		assertThat(result.getJobid()).isEqualTo(2L);
-		assertThat(result.getIdsJobId()).isEqualTo("ids-job-2");
-		assertThat(result.getUserid()).isEqualTo("ids-user-y");
-		assertThat(result.getStatus()).isEqualTo("results-deleted");
-		assertThat(result.getTotalrecs()).isEqualTo(348076);
-		assertThat(result.getRecssofar()).isEqualTo(2000);
-		assertThat(result.getStartdate()).isEqualTo(ldt2.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
 	}
 }
